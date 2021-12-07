8f7fdf7875a13a61d60d4ea4e1458a0c772ca167
npbasis <n.porubajmekh@basis.center>
2021-12-07 14:25:25 +0300

    installers

--- ironfish.sh
+++ ironfish.sh
@@ -1,215 +1,109 @@
 #!/bin/bash
-exists()
-{
-  command -v "$1" >/dev/null 2>&1
-}
-if exists curl; then
-	echo ''
-else
-  sudo apt install curl -y < "/dev/null"
-fi
-bash_profile=$HOME/.bash_profile
-if [ -f "$bash_profile" ]; then
-    . $HOME/.bash_profile
-fi
-sleep 1
+# Default variables
+function="install"
 
-function setupVars {
-	if [ ! $IRONFISH_WALLET ]; then
-		read -p "Enter wallet name: " IRONFISH_WALLET
-		echo 'export IRONFISH_WALLET='${IRONFISH_WALLET} >> $HOME/.bash_profile
-	fi
-	echo -e '\n\e[42mYour wallet name:' $IRONFISH_WALLET '\e[0m\n'
-	if [ ! $IRONFISH_NODENAME ]; then
-		read -p "Enter node name: " IRONFISH_NODENAME
-		echo 'export IRONFISH_NODENAME='${IRONFISH_NODENAME} >> $HOME/.bash_profile
-	fi
-	echo -e '\n\e[42mYour wallet name:' $IRONFISH_WALLET '\e[0m\n'
-	if [ ! $IRONFISH_THREADS ]; then
-		read -e -p "Enter your threads [-1]: " IRONFISH_THREADS
-		echo 'export IRONFISH_THREADS='${IRONFISH_THREADS:--1} >> $HOME/.bash_profile
-	fi
-	echo -e '\n\e[42mYour threads count:' $IRONFISH_THREADS '\e[0m\n'
-	echo "alias ironfish='yarn --cwd ~/ironfish/ironfish-cli/ start:once'" >> $HOME/.bash_profile && . $HOME/.bash_profile
-	echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
-	. $HOME/.bash_profile
-	alias ironfish='yarn --cwd ~/ironfish/ironfish-cli/ start:once'
-	sleep 1
-}
-
-function setupSwap {
-	echo -e '\n\e[42mSet up swapfile\e[0m\n'
-	grep -q "swapfile" /etc/fstab
-  if [[ ! $? -ne 0 ]]; then
-      echo -e '\n\e[42m[Swap] Swap file exist, skip.\e[0m\n'
-  else
-      cd $HOME
-      sudo fallocate -l 4G $HOME/swapfile
-      sudo dd if=/dev/zero of=swapfile bs=1K count=4M
-      sudo chmod 600 $HOME/swapfile
-      sudo mkswap $HOME/swapfile
-      sudo swapon $HOME/swapfile
-      sudo swapon --show
-      echo $HOME'/swapfile swap swap defaults 0 0' >> /etc/fstab
-      echo -e '\n\e[42m[Swap] Done\e[0m\n'
-  fi
-}
-
-function installDeps {
-	echo -e '\n\e[42mPreparing to install\e[0m\n' && sleep 1
-	cd $HOME
-	sudo apt update
-	sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
-	. $HOME/.cargo/env
-	curl https://deb.nodesource.com/setup_16.x | sudo bash
-	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-	# sudo apt upgrade -y < "/dev/null"
-	sudo apt install curl make clang pkg-config libssl-dev build-essential git jq nodejs -y < "/dev/null"
-	sudo npm --force install -g yarn
-}
-
-function createConfig {
-	echo "{
-		\"nodeName\": \"${IRONFISH_NODENAME}\",
-		\"blockGraffiti\": \"${IRONFISH_NODENAME}\"
-	}" > $HOME/.ironfish/config.json
-}
-
-function installSoftware {
-	. $HOME/.bash_profile
-	. $HOME/.cargo/env
-	echo -e '\n\e[42mInstall software\e[0m\n' && sleep 1
-	cd $HOME
-	git clone https://github.com/iron-fish/ironfish
-	cd $HOME/ironfish
-	git pull
-	# git checkout 45d85b7916b72f7ad263491b0c7fb6916b2edabd
-	cargo install --force wasm-pack
-	yarn
-	cp $HOME/ironfish/ironfish-cli/bin/ironfish /usr/bin
-}
-
-function updateSoftware {
-	if [[ `service ironfishd-listener status | grep active` =~ "running" ]]; then
-	  sudo systemctl stop ironfishd-listener
-	  sudo systemctl disable ironfishd-listener
-	fi
-	sudo systemctl stop ironfishd ironfishd-miner
-	. $HOME/.bash_profile
-	. $HOME/.cargo/env
-	cp -r $HOME/.ironfish/accounts $HOME/ironfish_accounts_$(date +%s)
-	echo -e '\n\e[42mInstall software\e[0m\n' && sleep 1
-	# rm -r $HOME/.ironfish
-	cd $HOME
-	installDeps
-	rm -r ironfish
-	# git clone https://github.com/iron-fish/ironfish -b staging
-	git clone https://github.com/iron-fish/ironfish
-	cd $HOME/ironfish
-	# git reset --hard
-	# git pull origin staging
-	cargo install --force wasm-pack
-	yarn
-}
-
-function installService {
-echo -e '\n\e[42mRunning\e[0m\n' && sleep 1
-echo -e '\n\e[42mCreating a service\e[0m\n' && sleep 1
-
-echo "[Unit]
-Description=IronFish Node
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=/usr/bin/yarn --cwd $HOME/ironfish/ironfish-cli/ start start
-Restart=always
-RestartSec=10
-LimitNOFILE=10000
-[Install]
-WantedBy=multi-user.target
-" > $HOME/ironfishd.service
-echo "[Unit]
-Description=IronFish Miner
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=/usr/bin/yarn --cwd $HOME/ironfish/ironfish-cli/ start miners:start -t $IRONFISH_THREADS
-Restart=always
-RestartSec=10
-LimitNOFILE=10000
-[Install]
-WantedBy=multi-user.target
-" > $HOME/ironfishd-miner.service
-sudo mv $HOME/ironfishd.service /etc/systemd/system
-sudo mv $HOME/ironfishd-miner.service /etc/systemd/system
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
-EOF
-sudo systemctl restart systemd-journald
-sudo systemctl daemon-reload
-echo -e '\n\e[42mRunning a service\e[0m\n' && sleep 1
-sudo systemctl enable ironfishd ironfishd-miner
-sudo systemctl restart ironfishd ironfishd-miner
-echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
-if [[ `service ironfishd status | grep active` =~ "running" ]]; then
-  echo -e "Your IronFish node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice ironfishd status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
-else
-  echo -e "Your IronFish node \e[31mwas not installed correctly\e[39m, please reinstall."
-fi
-if [[ `service ironfishd-miner status | grep active` =~ "running" ]]; then
-  echo -e "Your IronFish Miner node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice ironfishd-miner status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
-else
-  echo -e "Your IronFish Miner node \e[31mwas not installed correctly\e[39m, please reinstall."
-fi
-. $HOME/.bash_profile
-}
-
-
-function deleteIronfish {
-	sudo systemctl disable ironfishd ironfishd-miner ironfishd-listener
-	sudo systemctl stop ironfishd ironfishd-miner ironfishd-listener
-	sudo rm -r $HOME/ironfish
-}
-
-PS3='Please enter your choice (input your option number and press enter): '
-options=("Install" "Upgrade" "Delete" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Install")
-            echo -e '\n\e[42mYou choose install...\e[0m\n' && sleep 1
-			setupVars
-			setupSwap
-			installDeps
-			installSoftware
-			createConfig
-			installService
-			#installListener
-			break
-            ;;
-    "Upgrade")
-            echo -e '\n\e[33mYou choose upgrade...\e[0m\n' && sleep 1
-			setupVars
-			updateSoftware
-			installService
-			#installListener
-			echo -e '\n\e[33mYour node was upgraded!\e[0m\n' && sleep 1
-			break
-            ;;
-		"Delete")
-            echo -e '\n\e[31mYou choose delete...\e[0m\n' && sleep 1
-			deleteIronfish
-			echo -e '\n\e[42mIronfish was deleted!\e[0m\n' && sleep 1
-			break
-            ;;
-        "Quit")
-            break
-            ;;
-        *) echo -e "\e[91minvalid option $REPLY\e[0m";;
-    esac
+# Options
+. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/colors.sh) --
+option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
+while test $# -gt 0; do
+	case "$1" in
+	-h|--help)
+		. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/logo.sh)
+		echo
+		echo -e "${C_LGn}Functionality${RES}: the script installs an Iron Fish node"
+		echo
+		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
+		echo
+		echo -e "${C_LGn}Options${RES}:"
+		echo -e "  -h, --help            show the help page"
+		echo -e "  -u, --update          update the node"
+		echo
+		echo
+		return 0 2>/dev/null; exit 0
+		;;
+	-u|--update)
+		function="update"
+		shift
+		;;
+	*|--)
+		break
+		;;
+	esac
 done
+
+# Functions
+printf_n(){ printf "$1\n" "${@:2}"; }
+install() {
+	sudo apt update
+	sudo apt upgrade -y
+	if [ ! -n "$iron_fish_moniker" ]; then
+		printf_n "${C_LGn}Enter a node moniker${RES}"
+		. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/miscellaneous/insert_variable.sh) -n iron_fish_moniker
+	fi
+	if [ ! -n "$iron_fish_wallet_name" ]; then
+		printf_n "${C_LGn}Enter a wallet name${RES}"
+		. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/miscellaneous/insert_variable.sh) -n iron_fish_wallet_name
+	fi
+	sudo apt install wget jq bc build-essential -y
+	. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/installers/docker.sh)
+	docker run -dit --name iron_fish_node --restart always --network host --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest
+	. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/miscellaneous/insert_variable.sh) -n ifn_log -v "docker logs iron_fish_node -fn 100" -a
+	. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/miscellaneous/insert_variable.sh) -n if_node_info -v ". <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/ironfish_ni.sh) -l RU 2> /dev/null" -a
+	. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/miscellaneous/insert_variable.sh) -n ironfish -v "docker exec -it iron_fish_node ironfish" -a
+	docker exec -it iron_fish_node ironfish config:set nodeName $iron_fish_moniker
+	docker exec -it iron_fish_node ironfish config:set blockGraffiti $iron_fish_moniker
+	docker restart iron_fish_node
+	printf_n "${C_LGn}Waiting 20 seconds...${RES}"
+	sleep 20
+	if ! docker exec -it iron_fish_node ironfish accounts:list | grep -q $iron_fish_wallet_name && [ ! -f $HOME/iron_fish_${iron_fish_wallet_name}.json ]; then
+		docker exec -it iron_fish_node ironfish accounts:create $iron_fish_wallet_name
+		docker exec -it iron_fish_node ironfish accounts:export $iron_fish_wallet_name "iron_fish_${iron_fish_wallet_name}.json"
+		docker cp iron_fish_node:/usr/src/app/iron_fish_${iron_fish_wallet_name}.json $HOME/iron_fish_${iron_fish_wallet_name}.json
+	elif [ -f $HOME/iron_fish_${iron_fish_wallet_name}.json ]; then
+		docker cp $HOME/iron_fish_${iron_fish_wallet_name}.json iron_fish_node:/usr/src/app/iron_fish_${iron_fish_wallet_name}.json
+		docker exec -dit iron_fish_node ironfish accounts:import "iron_fish_${iron_fish_wallet_name}.json"
+		docker exec -dit iron_fish_node ironfish accounts:use $iron_fish_wallet_name
+	fi
+	docker exec -it iron_fish_node ironfish accounts:use $iron_fish_wallet_name
+	printf_n "${C_LGn}Done!${RES}"
+	. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/logo.sh)
+	printf_n "
+The node was ${C_LGn}started${RES}.
+Remember to save this file: ${C_LR}$HOME/iron_fish_${iron_fish_wallet_name}.json${RES}
+
+\tv ${C_LGn}Useful commands${RES} v
+
+To view info about the node: ${C_LGn}if_node_info${RES}
+Alias for node commands execution: ${C_LGn}ironfish${RES}
+To view the node log: ${C_LGn}ifn_log${RES}
+To restart the node: ${C_LGn}docker restart iron_fish_node${RES}
+"
+}
+update() {
+	printf_n "${C_LGn}Checking for update...${RES}"
+	status=`docker pull ghcr.io/iron-fish/ironfish:latest`
+	if ! grep -q "Image is up to date for" <<< "$status"; then
+		printf_n "${C_LGn}Updating...${RES}"
+		if docker ps -a | grep -q iron_fish_node; then
+			docker stop iron_fish_node
+			docker rm iron_fish_node
+			docker run -dit --name iron_fish_node --restart always --network host --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest
+			printf_n "${C_LGn}The node was successfully updated!${RES}"
+		fi
+		if docker ps -a | grep -q iron_fish_miner; then
+			local threads=`docker inspect iron_fish_miner | jq -r ".[0].Config.Cmd[2]"`
+			docker stop iron_fish_miner
+			docker rm iron_fish_miner
+			docker run -dit --name iron_fish_miner --restart always --network host --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest miners:start -t $threads
+			printf_n "${C_LGn}The miner was successfully updated!${RES}"
+		fi
+	else
+		printf_n "${C_LGn}Node version is current!${RES}"
+	fi
+}
+
+# Actions
+sudo apt install wget jq -y &>/dev/null
+. <(wget -qO- https://raw.githubusercontent.com/Kallen-c/utils/main/logo.sh)
+if [ -f $HOME/iron_fish_${iron_fish_wallet_name}.txt ]; then mv $HOME/iron_fish_${iron_fish_wallet_name}.txt $HOME/iron_fish_${iron_fish_wallet_name}.json; fi
+$function
