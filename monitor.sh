1fa29f44e23b0f24065afa700faddce5e69fc376
npbasis <n.porubajmekh@basis.center>
2022-01-10 16:15:53 +0300

    installers cel

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 #todo: sudo apt-get update && sudo apt-get install prometheus-node-exporter
+read -p "Введите название своего сервера: " HOSTNAMEx
+echo 'Название вашего сервера: ' $HOSTNAMEx
+sleep 1
+echo 'export HOSTNAMEx='$HOSTNAMEx >> $HOME/.profile
+sudo hostname $HOSTNAMEx
+sleep 1
+
 sudo apt-get update && sudo apt-get install prometheus-node-exporter
+
 
 #wget -qO- https://repos.influxdata.com/influxdb.key | sudo tee /etc/apt/trusted.gpg.d/influxdb.asc >/dev/null
 #source /etc/os-release
